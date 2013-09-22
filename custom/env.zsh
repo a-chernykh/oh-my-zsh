847157dc211313600509794a9028b0e231746a5b
Andrey Chernih <andrey.chernih@gmail.com>
2013-09-22 20:34:11 +0400

    Add Android paths

--- custom/env.zsh
+++ custom/env.zsh
@@ -1,2 +1,6 @@
 export GIT_EDITOR='vim'
 export EDITOR='subl'
+export ANDROID_HOME='/opt/android/adt-bundle-mac-x86_64-20130522/sdk'
+export ANDROID_NDK_HOME='/opt/android/android-ndk-r9'
+export OPENCV_SDK_HOME='/opt/android/adt-bundle-mac-x86_64-20130522/OpenCV-2.4.6-android-sdk'
+export PATH="$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$ANDROID_NDK_HOME"
