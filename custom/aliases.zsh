529247a81bae3cbf5746921d9b326b377109bb53
Andrey Chernih <andrey.chernih@gmail.com>
2013-10-25 15:38:20 +0400

    Add gaa and gt aliases

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -6,6 +6,7 @@ alias h='history'
 
 # git aliases
 alias ga='git add'
+alias gaa='git add --all'
 alias gl='git pull'
 alias gp='git push'
 alias gd='git diff'
@@ -16,6 +17,7 @@ alias gb='git branch'
 alias gs='git status -sb'
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
 alias glg="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+alias gt='git tag'
 
 # rails aliases
 alias r='rails'
