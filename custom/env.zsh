fc13004bb1c9bf990c3a135839449512cffc87cd
Andrey Chernih <andrey.chernih@gmail.com>
2013-11-08 13:02:43 +0400

    Add /usr/local/sbin to PATH

--- custom/env.zsh
+++ custom/env.zsh
@@ -3,4 +3,4 @@ export EDITOR='subl'
 export ANDROID_HOME='/opt/android/android-sdk-macosx'
 export ANDROID_NDK_HOME='/opt/android/android-ndk-r9'
 export OPENCV_SDK_HOME='/opt/android/OpenCV-2.4.6-android-sdk'
-export PATH="$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$ANDROID_NDK_HOME"
+export PATH="$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$ANDROID_NDK_HOME:/usr/local/sbin"
