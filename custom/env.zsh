7a652f808dc9b20d96a9964d3686464cc8977057
Andrey Chernih <andrey.chernih@gmail.com>
2013-11-19 22:00:32 +0400

    Add -w flag for subl so that it works with in-place editing

--- custom/env.zsh
+++ custom/env.zsh
@@ -1,4 +1,4 @@
-export EDITOR='subl'
+export EDITOR='subl -w'
 export ANDROID_HOME='/opt/android/android-sdk-macosx'
 export ANDROID_NDK_HOME='/opt/android/android-ndk-r9'
 export OPENCV_SDK_HOME='/opt/android/OpenCV-2.4.6-android-sdk'
