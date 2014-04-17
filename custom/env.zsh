1ea112c870068b930132951f732d56c31e621ad5
Andrey Chernih <andrey.chernih@gmail.com>
2014-04-17 22:27:38 +0400

    Add some paths

--- custom/env.zsh
+++ custom/env.zsh
@@ -1,5 +1,7 @@
-export EDITOR='subl -w'
+export EDITOR='/Users/akhkharu/bin/subl'
+export GIT_EDITOR='/Users/akhkharu/bin/subl -w'
 export ANDROID_HOME='/opt/android/android-sdk-macosx'
 export ANDROID_NDK_HOME='/opt/android/android-ndk-r9'
 export OPENCV_SDK_HOME='/opt/android/OpenCV-2.4.6-android-sdk'
+export TBB_PATH='/opt/android/tbb42_20140122oss'
 export PATH="$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$ANDROID_NDK_HOME:/usr/local/sbin"
