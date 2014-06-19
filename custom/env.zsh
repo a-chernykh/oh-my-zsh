3c191966e35dcd97d4304734acbabfbfab5f4ae4
Andrey Chernih <andrey.chernih@gmail.com>
2014-06-19 14:35:32 +0400

    Add JAVA_HOME

--- custom/env.zsh
+++ custom/env.zsh
@@ -5,3 +5,4 @@ export ANDROID_NDK_HOME='/opt/android/android-ndk-r9'
 export OPENCV_SDK_HOME='/opt/android/OpenCV-2.4.6-android-sdk'
 export TBB_PATH='/opt/android/tbb42_20140122oss'
 export PATH="$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$ANDROID_NDK_HOME:/usr/local/sbin"
+export JAVA_HOME='/Library/Java/JavaVirtualMachines/jdk1.7.0_25.jdk/Contents/Home'
