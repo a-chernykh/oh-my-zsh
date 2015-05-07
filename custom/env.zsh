db8648114df6b0cbac9e4898e1c6efb6e0249952
Andrey Chernih <andrey.chernih@gmail.com>
2015-05-06 22:08:24 -0700

    vim

--- custom/env.zsh
+++ custom/env.zsh
@@ -1,5 +1,4 @@
-export EDITOR='/Users/akhkharu/bin/subl'
-export GIT_EDITOR='/Users/akhkharu/bin/subl -w'
+export EDITOR='vim'
 export ANDROID_HOME='/opt/android/android-sdk-macosx'
 export ANDROID_NDK_HOME='/opt/android/android-ndk-r9'
 export OPENCV_SDK_HOME='/opt/android/OpenCV-2.4.6-android-sdk'
@@ -8,7 +7,7 @@ export TBB_PATH='/opt/android/tbb42_20140122oss'
 export EC2_HOME='/usr/local/ec2/ec2-api-tools-1.7.1.1'
 export AWS_ELB_HOME='/usr/local/elb/ElasticLoadBalancing-1.0.35.0'
 
-export GOPATH='/usr/local/opt/go/libexec'
+export GOPATH='/usr/local/opt/go'
 export PATH="$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$ANDROID_NDK_HOME:/usr/local/sbin:$EC2_HOME/bin:$AWS_ELB_HOME/bin:$GOPATH/bin"
 export JAVA_HOME='/Library/Java/JavaVirtualMachines/jdk1.7.0_25.jdk/Contents/Home'
 
